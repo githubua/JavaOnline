74fedf692d3d8bd9e41153a1f4a0d1bcff344a5f
githubua <interni7@gmail.com>
2016-03-17 19:32:40 +0200

    first relise

---
+++ src/ua/go/module1/task1/Asd.java
@@ -0,0 +1,9 @@
+package ua.go.module1.task1;
+
+/**
+ * Created by admin on 17.03.2016.
+ */
+public class Asd {
+    public static void main(String[] args) {
+        System.out.println(" Hello Java");
+}
---
+++ src/ua/go/module1/task1/Qwer.java
@@ -0,0 +1,10 @@
+package ua.go.module1.task1;
+
+/**
+ * Created by admin on 17.03.2016.
+ */
+public class Qwer {
+    public static void main(String[] args) {
+        System.out.println(" Hello");
+    }
+}
